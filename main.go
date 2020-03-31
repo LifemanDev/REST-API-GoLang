95aecfdc967a57d0d5cfbc460a8bbf39d4ac2f8f
Nais <gunnar_holten@hotmail.com>
2020-03-31 11:57:21 +0200

    Add files via upload

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("", get).Methods(http.MethodGet)
+	api.HandleFunc("", post).Methods(http.MethodPost)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`
+		{
+			"Titel": "LET'S GO!",
+			"message": "GoLang :)"
+		}
+	`))
+}
+
+func post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(`
+		{
+			"Titel": "EPIC POST!",
+			"message": "damn"
+		}
+	`))
+}
